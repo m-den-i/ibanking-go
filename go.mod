92be11a54cb560b239702b16bfebcbcf4197c101
Denis Makedonov <denis.makedonov@gmail.com>
2021-03-05 17:54:04 +0300

    print data #2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/m-den-i/ibanking-go
 
 go 1.15
+
+require github.com/PuerkitoBio/goquery v1.6.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type stringline []string
 
 func main() {
-	fmt.Println("Hello, world!")
+	f, err := os.Open("report.html")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	rio := bufio.NewReader(f)
+	doc, err := goquery.NewDocumentFromReader(rio)
+	if err != nil {
+		panic(err)
+	}
+	var x []stringline
+	doc.Find(".section_3 tr").Each(func(i int, s *goquery.Selection) {
+		var t []string
+		for _, l := range strings.Split(s.Text(), "\n") {
+			if l != "" {
+				t = append(t, l)
+			}
+		}
+		if len(t) > 0 {
+			x = append(x, t)
+		}
+	})
+	for _, l := range x[0:20] {
+		fmt.Println(len(l), l)
+	}
 }
